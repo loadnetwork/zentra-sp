9cf10b18712797dbd3d9ab5244cfffd24bbf4d45
charmful0x <charmful0x@gmail.com>
2025-07-06 18:37:58 +0200

    feat: deserialize zentra fn tx input

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 pub mod utils;
+use crate::utils::eth_provider::get_zentra_tx_data;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Hello, world!");
+    let data = get_zentra_tx_data("0x469548e90a48718d2f4aacf8d4d7b4e6c66563b83fdd6bd193927321d7816355").await.unwrap();
+    println!("{:?}", data);
 }
--- src/utils/eth_provider.rs
+++ src/utils/eth_provider.rs
@@ -1,18 +1,26 @@
 use alloy::providers::{ProviderBuilder, Provider};
 use alloy::primitives::{address, Bytes, TxHash};
 use anyhow::{anyhow, Error};
+use serde_json::Value;
 use std::str::FromStr;
 
 use crate::utils::constants::BASE_SEPOLIA_RPC;
 
-
+// todo: validate senders
 pub async fn get_transaction_data(txid: &str) -> Result<alloy::primitives::Bytes, Error> {
     let txid = TxHash::from_str(txid)?;
     let provider = ProviderBuilder::new().connect_http(BASE_SEPOLIA_RPC.parse()?);
     let tx = provider.get_transaction_by_hash(txid.into()).await?;
     let tx_bytes: Option<alloy::consensus::EthereumTxEnvelope<alloy::consensus::TxEip4844Variant>> = tx.and_then(|txb| txb.into_inner().try_into().ok());
-    let tx_envelope = tx_bytes.ok_or("").map_err(|err| anyhow!(err))?;
-    let tx_eip1559 = tx_envelope.as_eip1559().ok_or("").map_err(|err| anyhow!(err))?;
+    let tx_envelope = tx_bytes.ok_or("error accessing tx envelope").map_err(|err| anyhow!(err))?;
+    let tx_eip1559 = tx_envelope.as_eip1559().ok_or("error converting envelop to eip1559").map_err(|err| anyhow!(err))?;
     let res = tx_eip1559.clone().into_parts().0.input;
     Ok(res)
+}
+
+pub async fn get_zentra_tx_data(txid: &str) -> Result<Value, Error> {
+    let input = get_transaction_data(txid).await?;
+    let hex_input = String::from_utf8(input.into())?;
+    let json_input = serde_json::from_str(&hex_input)?;
+    Ok(json_input)
 }
