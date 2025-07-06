3ed77aec47b1e27f24f93aff04eeca02f7a4dbd2
charmful0x <charmful0x@gmail.com>
2025-07-06 18:49:38 +0200

    feat: load0 storage pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 pub mod utils;
 use crate::utils::eth_provider::get_zentra_tx_data;
+use crate::utils::load0::upload_to_load0;
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
     let data = get_zentra_tx_data("0x469548e90a48718d2f4aacf8d4d7b4e6c66563b83fdd6bd193927321d7816355").await.unwrap();
-    println!("{:?}", data);
+    let load_hash = upload_to_load0(data.to_string().as_bytes().to_vec(), "application/json").await.unwrap();
+    println!("{:?} {:?}", data, load_hash);
 }
---
+++ src/utils/load0.rs
@@ -0,0 +1,42 @@
+use crate::utils::constants::LOAD0_ENDPOINT_URL;
+use anyhow::{Error, anyhow};
+use reqwest::Client;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Load0UploadResponse {
+    pub optimistic_hash: String,
+    pub success: bool,
+}
+
+// todo: load_acc api key
+pub async fn upload_to_load0(data: Vec<u8>, content_type: &str) -> Result<String, Error> {
+    let client = Client::new();
+
+    let upload_url = format!("{}/upload", LOAD0_ENDPOINT_URL);
+
+    let response = client
+        .post(&upload_url)
+        .header("Content-Type", content_type)
+        .body(data)
+        .send()
+        .await?;
+
+    if !response.status().is_success() {
+        return Err(anyhow!(
+            "Failed to upload to Load0: Status {}, Body: {}",
+            response.status(),
+            response.text().await?
+        ));
+    }
+
+    let upload_response = response.json::<Load0UploadResponse>().await?;
+
+    if (upload_response.success) {
+        return Ok(upload_response.optimistic_hash);
+    }
+
+    Ok(String::from(
+        "0x0000000000000000000000000000000000000000000000000000000000000000",
+    ))
+}
